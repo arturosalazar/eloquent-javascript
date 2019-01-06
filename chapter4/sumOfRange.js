3cae85001241a603077963188f48d43b7549feb7
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2019-01-05 19:57:40 -0600

    Allow for negative and positive steps
    provided a step variable that defaults to 0
    If the user provides a negative step value, count down

--- chapter4/sumOfRange.js
+++ chapter4/sumOfRange.js
@@ -5,12 +5,26 @@
   Next, write a sum function that takes an array of numbers and returns the
   sum of these numbers. Run the example program and see whether it does
   indeed return 55.
+  
+  As a bonus assignment, modify your range function to take an optional third
+  argument that indicates the “step” value used when building the array. If no
+  step is given, the elements go up by increments of one, corresponding to the
+  old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7,
+  9]. Make sure it also works with negative step values so that range(5, 2, -1)
+  produces [5, 4, 3, 2].
 */
 
-function range (start, finish){
+function range (start, finish, step = 1){  
   let rangeArray = [];
-  for (i = start; i <= finish; i++){
-    rangeArray.push(i);
+  if (start < finish){
+    for (i = start; i <= finish; i+=step){
+      rangeArray.push(i);
+    }
+  }  
+  else {
+    for (i = start; i >= finish; i+=step){
+      rangeArray.push(i);
+    }
   }
   return rangeArray;
 }
@@ -20,7 +34,7 @@ function sum (array){
   for (let i = 0; i < array.length; i++){
     sum += array[i];
   }
-  return sum;
+  return sum
 }
 
-console.log(sum(range(1,10)));
+console.log (sum(range(1,10)));
