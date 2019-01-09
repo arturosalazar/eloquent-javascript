df1603650c728db438a0cb8b2657b511b5f0970b
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2019-01-08 23:16:11 -0600

    nthR changed to nthRecursive
    changed nthR to a more descriptive name - nthRecursive

--- chapter4/aList.js
+++ chapter4/aList.js
@@ -60,10 +60,10 @@ function nth(list, number){
 
 //nth - recursive
 //use number as a counter, and if number > 0, call nthR again and check the next inner object
-function nthR (list, number){
+function nthRecursive (list, number){
   if (number == 0) return list.value;
   
   if (list.rest === null && number > 0) return undefined;
 
-  if (number > 0) return nthR(list.rest, number-1); 
+  if (number > 0) return nthRecursive(list.rest, number-1); 
 }
