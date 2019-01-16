55165a27f59b71631bda28484bd7b03b9b1d074a
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2019-01-15 21:30:21 -0600

    Update dominantWritingDirection.js

--- chapter5/dominantWritingDirection.js
+++ chapter5/dominantWritingDirection.js
@@ -35,7 +35,8 @@ function characterScript(code) {
   return null;
 }
 
-
+//count the number of items belonging to a group calculated with the function groupName
+//returns the group and count pairings as an object in an array
 function countBy(items,groupName) {
   let counts = [];
   for (let item of items) {
@@ -51,19 +52,25 @@ function countBy(items,groupName) {
 }
 
 function dominantDirection(text) {
-  let scripts = countBy(text, char => {
+  //gather array of objects with script directions and their counts in text
+  let directions = countBy(text, char => {
     let script = characterScript(char.codePointAt(0));
     return script ? script.direction : "none";
   }).filter(({name}) => name != "none");
 
-  let total = scripts.reduce((n, {count}) => n + count, 0);
+  //find the total number of directions 
+  let total = directions.reduce((n, {count}) => n + count, 0);
   if (total == 0) return "No scripts found";
 
-  scripts = scripts.map(({name, count}) => {
-    return {name:name, count:Math.round(count * 100 / total)};
-  })
+  //caluculate percentage of the text each direction takes
+  //put that information in a percentage property
+  directions = directions.map(({name, count}) => {
+    return {name:name, percentage:Math.round(count * 100 / total)};
+  });
 
-  return scripts.reduce((a,b) => {
-    return a.count < b.count ? b : a;
+  //use reduce to find the direction with the largest percentage
+  //return that direction's name property
+  return directions.reduce((a,b) => {
+    return a.percentage < b.percentage ? b : a;
   }).name;
 }
