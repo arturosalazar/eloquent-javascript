fa0ca2f8ea2c09ed469ad9cedb2c51ab4df2541b
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2019-01-15 19:57:19 -0600

    Create flattening.js

---
+++ chapter5/flattening.js
@@ -0,0 +1,17 @@
+/*
+  Use the reduce method in combination with the concat method to “flatten”
+  an array of arrays into a single array that has all the elements of
+  the original arrays.
+*/
+
+let arrays = [[1, 2, 3], [4, 5], [6]];
+let reducer = (accumulator, currentValue) => {
+  for (elements of currentValue) {
+    accumulator.push(elements);
+  }
+  return accumulator;
+}
+
+arrays.reduce(reducer, [])
+
+// → [1, 2, 3, 4, 5, 6]
