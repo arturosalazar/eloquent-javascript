1a950862c83839700692724f3aa713dad3c55952
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2019-01-28 20:23:59 -0600

    Create groups.js
    work in progress

---
+++ chapter6/groups.js
@@ -0,0 +1,42 @@
+/*
+  A set holds a collection of values but does not associate other values with
+  those - just tracks which values are part of the set. A value can be part 
+  of a set only once—adding it again doesn’t have any effect.
+  
+  Write a class called Group (since Set is already taken). 
+
+  Like Set, it has add, delete, and has methods. 
+  Its constructor creates an empty group, 
+  add adds a value to the group (but only if it isn’t already a member), 
+  delete removes its argument from the group (if it was a member), and 
+  has returns a Boolean value indicating whether its argument is a member of 
+  the group.
+  Use the === operator, or something equivalent such as indexOf, to determine
+  whether two values are the same.
+
+  Give the class a static from method that takes an iterable object as argument
+  and creates a group that contains all the values produced by iterating over it.
+*/
+
+//class group
+class Group {
+  
+  //constructor - empty group (array?)
+  constructor () {
+      this.group = [];
+  }
+
+  //has Boolena to see if the arg is memeber of the group...maybe make first
+  //so we can use for add?
+
+  //add - add value to the group ONLY if it isn't already a member
+    //indexOf? to check if it's in the group. Or a for loop to iterate over
+    //and check with ===
+
+  //delete - find the value and delete it from the Group
+
+  //static - take iterable object as agrument
+    //create a group that contains all the values produced by iterating over it??
+    //for of? to iterate?
+}
+  
