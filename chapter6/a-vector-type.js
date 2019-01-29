112511923491eea8942e32e54d9f9af19c427627
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2019-01-28 20:09:07 -0600

    added getter length
    added getter length - final output satisfies the requirements

--- chapter6/a-vector-type.js
+++ chapter6/a-vector-type.js
@@ -7,36 +7,37 @@
   of the two vectors’ (this and the parameter) x and y values.
   Add a getter property length to the prototype that computes the length of
   the vector—that is, the distance of the point (x, y) from the origin (0, 0).
+*/
 
-// Goal output:
+//Create a class called Vec
+class Vec {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 
+  //take another vector obj as input - return a new vector with sum
+  //of the vectors' x and y values
+  plus(vecObject) {
+    return new Vec((this.x + vecObject.x), (this.y + vecObject.y));
+  }
+
+  //take another vector obj as input - return a new vector with difference
+  //of the vectors' x and y values
+  minus(vecObject) {
+    return new Vec((this.x - vecObject.x), (this.y - vecObject.y));
+  }
+
+  //create a getter to give distance between vector and 0,0
+  get length() {
+    return Math.sqrt(Math.pow(0 - this.x, 2) + Math.pow(0 - this.y, 2))
+  }
+}
+
+//'tests' and the desired results
 console.log(new Vec(1, 2).plus(new Vec(2, 3)));
 // → Vec{x: 3, y: 5}
 console.log(new Vec(1, 2).minus(new Vec(2, 3)));
 // → Vec{x: -1, y: -1}
 console.log(new Vec(3, 4).length);
 // → 5
-*/
-
-//Create a class called Vec
-class Vec{
-  constructor(x,y) {
-    this.x = x;
-    this.y = y;
-  }
-
-  //take another vector obj as input - return a new vector with sum 
-  //of the vectors' x and y values
-  plus(vecObject){
-    return new Vec ((this.x+vecObject.x),(this.y+vecObject.y));
-  }
-
-  //take another vector obj as input - return a new vector with difference 
-  //of the vectors' x and y values
-  minus(vecObject){
-    return new Vec ((this.x-vecObject.x),(this.y-vecObject.y));
-  }
-
-  //getter - length of vector - dist between this and 0
-
-}
